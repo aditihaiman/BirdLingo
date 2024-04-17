86c1ef08c26137597d226902d06bb5e2bd54165e
mosheeboy <milesallenblackwell@gmail.com>
2024-04-17 12:22:38 -0400

    added pextra learning pages from the beginning of our deck

--- static/learn.js
+++ static/learn.js
@@ -11,26 +11,40 @@ $(function () {
 
 });
 
+
 function navigate_next() {
+    let currentId = parseInt(window.location.pathname.split('/').pop());
     if (window.location.href.includes('learn')) {
-        //if at learn/3 go to quiz template
-        if (window.location.href.includes('learn/3')) {
-            //FILL IN WHOEVER IS DOING QUIZ
-            window.location.href = "/quiz/0"
+        if (window.location.href.includes('learn_checkpoint')) {
+            // Redirect to the second learning page
+            window.location.href = "/learn/2";
+        } else if (window.location.href.includes('learn/2')) {
+            // Redirect to the checkpoint page after the second learning page
+            window.location.href = "/learn_checkpoint";
+        } else if (currentId === 3) { // Assuming 4 is the ID of the last learning slide
+            // Redirect to the quiz
+            window.location.href = "/quiz/0";
+        } else {
+            // Navigate to the next learning slide
+            window.location.href = "/learn/" + (currentId + 1);
         }
-        else window.location.href = "/learn/" + (Number(learn_data['id']) + 1);
-    }
-    else {
+    } else {
         window.location.href = "/learn/0";
     }
 }
 
+
+
+
+
 function navigate_prev() {
+    let currentId = parseInt(window.location.pathname.split('/').pop());
     if (window.location.href.includes('learn')) {
-        //if at learn/0 go to prev template
+        // If at learn/0, go to prev template (fill in as needed)
         if (window.location.href.includes('learn/0')) {
-            //FILL IN WHOEVER IS DOING MISCELLANEOUS
+            // FILL IN WHOEVER IS DOING MISCELLANEOUS
+        } else {
+            window.location.href = "/learn/" + (currentId - 1);
         }
-        else window.location.href = "/learn/" + (Number(learn_data['id']) - 1);
     }
 }
