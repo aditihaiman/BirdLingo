4227547d31d37b6cc719cf0659cf0af0136ce31b
mosheeboy <milesallenblackwell@gmail.com>
2024-05-02 22:50:39 -0400

    working learning_checkpoint_layout.html

--- static/learn.js
+++ static/learn.js
@@ -1,76 +1,92 @@
 $(function () {
+    // Navigation buttons
     $("#next-button").on('click', navigate_next);
     $("#prev-button").on('click', navigate_prev);
     $("#home-learn").on('click', navigate_next);
-    $("#spectrogram").on('click', function(){
+
+    // Toggling information dialogs
+    $("#spectrogram").on('click', function() {
         $("#spectro-info").toggleClass('d-none');
     });
-    $("#characteristics").on('click', function(){
+    $("#characteristics").on('click', function() {
         $("#char-info").toggleClass('d-none');
     });
 
-    $( "#char-info" ).dialog({
+    // Initialize dialogs
+    $("#char-info, #spectro-info").dialog({
         autoOpen: false,
         draggable: true,
         show: {
-          effect: "blind",
-          duration: 500
+            effect: "blind",
+            duration: 500
         },
         hide: {
-          effect: "blind",
-          duration: 500
+            effect: "blind",
+            duration: 500
         }
-      });
-   
-      $( "#characteristics" ).on( "click", function() {
-        if($("#char-info").dialog("isOpen")){
-            $( "#char-info" ).dialog( "close" );
-        }
-        else{
-            $( "#char-info" ).dialog( "open" );
-        }
-        
-      });
+    });
 
-      $( "#spectro-info" ).dialog({
-        autoOpen: false,
-        draggable: true,
-        show: {
-          effect: "blind",
-          duration: 500
+    // Event to open/close dialog on button click
+    $("#characteristics, #spectrogram").on("click", function() {
+        var targetId = $(this).data("target");
+        var targetDialog = $("#" + targetId);
+        if (targetDialog.dialog("isOpen")) {
+            targetDialog.dialog("close");
+        } else {
+            targetDialog.dialog("open");
+        }
+    });
+
+    // Drag and Drop functionality
+    $(".draggable").draggable({
+        revert: "invalid",
+        start: function() {
+            $(this).css("opacity", 0.5);
         },
-        hide: {
-          effect: "blind",
-          duration: 500
+        stop: function() {
+            $(this).css("opacity", 1);
         }
-      });
-   
-      $( "#spectrogram" ).on( "click", function() {
-        if($("#spectro-info").dialog("isOpen")){
-            $( "#spectro-info" ).dialog( "close" );
+    });
+
+    $(".droppable").droppable({
+        accept: ".draggable",
+        classes: {
+            "ui-droppable-hover": "ui-state-hover"
+        },
+        drop: function(event, ui) {
+            var dropZoneBird = $(this).data('bird');
+            var draggedBird = ui.draggable.attr('id');
+
+            console.log("Drop Zone expects:", dropZoneBird); // Debugging
+            console.log("Dragged Item ID:", draggedBird); // Debugging
+
+            if (draggedBird === dropZoneBird) {
+                $(this).append(ui.draggable);
+                ui.draggable.position({
+                    my: "center",
+                    at: "center",
+                    of: $(this)
+                });
+                ui.draggable.css("margin", 0);
+                alert("Correct! You placed the " + draggedBird + " in the right zone.");
+            } else {
+                $(ui.draggable).css({ top: "0px", left: "0px" }); // Reset position
+                alert("Incorrect. Try again!");
+            }
         }
-        else{
-            $( "#spectro-info" ).dialog( "open" );
-        }
-        
-      });
+    });
 });
-
 
 function navigate_next() {
     let currentId = parseInt(window.location.pathname.split('/').pop());
     if (window.location.href.includes('learn')) {
         if (window.location.href.includes('learn_checkpoint')) {
-            // Redirect to the second learning page
             window.location.href = "/learn/4";
         } else if (window.location.href.includes('learn/3')) {
-            // Redirect to the checkpoint page after the second learning page
             window.location.href = "/learn_checkpoint";
-        } else if (currentId === 5) { // Assuming 4 is the ID of the last learning slide
-            // Redirect to the quiz
+        } else if (currentId === 5) { // Assuming 4 is the last learning slide
             window.location.href = "/transition";
         } else {
-            // Navigate to the next learning slide
             window.location.href = "/learn/" + (currentId + 1);
         }
     } else {
@@ -78,13 +94,11 @@ function navigate_next() {
     }
 }
 
-
 function navigate_prev() {
-    let currentId = parseInt(window.location.pathname.split('/').pop());
+    let currentId = parseInt(window.location.pathname.split('/'). pop());
     if (window.location.href.includes('learn')) {
-        // If at learn/0, go to prev template (fill in as needed)
         if (window.location.href.includes('learn/0')) {
-            // FILL IN WHOEVER IS DOING MISCELLANEOUS
+            // Fill in whoever is handling miscellaneous
         } else if (window.location.href.includes('learn/4')) {
             window.location.href = '/learn_checkpoint';
         } else {
